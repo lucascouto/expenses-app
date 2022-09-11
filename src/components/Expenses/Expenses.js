6766e95693860e2651625faf9d89ee074b8d5328
Lucas Couto <loc.unb@gmai.com>
2022-09-11 00:17:27 -0300

    Creates a component to return the expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
 
 import "./Expenses.css";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -16,19 +16,6 @@ const Expenses = (props) => {
     (expense) => expense.date.getFullYear().toString() === filteredYear
   );
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <>
       <Card className="expenses">
@@ -36,7 +23,7 @@ const Expenses = (props) => {
           onFilterChange={changeFilterHandler}
           selectedYear={filteredYear}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </>
   );
