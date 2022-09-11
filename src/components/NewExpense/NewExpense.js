a3ec040d2616f819ef4835feadda1277f2dab12d
Lucas Couto <loc.unb@gmai.com>
2022-09-11 00:50:24 -0300

    Hide/show form to add a new expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpanseHandler = (expenseData) => {
+    setIsEditing(false);
+
     const expense = {
       ...expenseData,
       id: Math.random().toString(),
@@ -11,9 +16,24 @@ const NewExpense = (props) => {
     props.onAddExpense(expense);
   };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpense={saveExpanseHandler} />
+      {isEditing ? (
+        <ExpenseForm
+          onSaveExpense={saveExpanseHandler}
+          onCancel={stopEditingHandler}
+        />
+      ) : (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
